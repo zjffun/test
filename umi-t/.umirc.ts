f70bf4135675214300425ba2b344ee4b013af1c3
JuFeng Zhang <zjffun@gmail.com>
2022-03-05 19:09:12 +0800

    umi requirejs

--- umi-t/.umirc.ts
+++ umi-t/.umirc.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  devtool: 'source-map',
+  chainWebpack(memo, { env, webpack, createCSSRule }) {
+    // COMPRESS=none
+    // memo.optimization.minimize(false);
+
+    // use this will not auto run
+    // memo.output.umdNamedDefine(true);
+
+    // use qiankun with requirejs
+    memo.output.libraryTarget('window');
+  },
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [{ path: '/', component: '@/pages/index' }],
+  routes: [{ path: '*', component: '@/pages/index' }],
   qiankun: {
     slave: {},
   },
