0a6e34f6af5f8b323cc7955749b1e94a98e647ce
Jufeng Zhang <zjffun@gmail.com>
2022-07-30 15:03:24 +0800

    esm

--- esm-t/esm-require.js
+++ esm-t/esm-require.js
@@ -3,6 +3,7 @@ const esmRequire = require("esm")(module, {
   force: true,
 });
 
+// Error [ERR_REQUIRE_ESM]: require() of ES Module xxx not supported.
 const chalk = esmRequire("chalk");
 
 console.log(chalk);
